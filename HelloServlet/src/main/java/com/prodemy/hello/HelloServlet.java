9818ec87abc231a6951096c1c14d4461b89b4f5b
Awiyanto <awiyanto.ajisasongko@gmail.com>
2023-03-02 10:25:29 +0700

    Tambah komen di HelloServlet

--- HelloServlet/src/main/java/com/prodemy/hello/HelloServlet.java
+++ HelloServlet/src/main/java/com/prodemy/hello/HelloServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * HelloServlet
+ * @author Awiyanto
+ *
+ */
 @WebServlet("/hello.html")
 public class HelloServlet extends HttpServlet {
 
